d667a560a7a7ed8467e46f457ff2ee5a7a9689e8
VladimirMironov <44780782+vladimirVFX@users.noreply.github.com>
2020-04-28 18:45:16 +0300

    Add files via upload

---
+++ DinamikMassive.cpp
@@ -0,0 +1,32 @@
+﻿// DinamikMassive.cpp 
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251>nul");
+    int* size;
+    size = new int;
+    
+    cout << "Enter massive size = ";
+    cin >> *size;
+
+    char* symbs;
+    symbs = new char[*size];
+
+    for (int k = 0; k < *size; k++) {
+        symbs[k] = 'a' + k;
+        cout << symbs[k] << " ";
+    }
+
+    delete[] symbs;
+    delete size;
+
+    cout << "\nMassive and variable are deleted\n";
+    
+    system("pause>nul");
+    return 0;
+}
